dba092beba4b58d918bc7b13fcd6557ac6c50629
Juanes07 <juanesdm3@gmail.com>
2022-07-13 15:34:06 -0500

    Proyecto base inicial

---
+++ cicilismo/src/main/java/com/sofka/cicilismo/CicilismoApplication.java
@@ -0,0 +1,13 @@
+package com.sofka.cicilismo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicilismoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicilismoApplication.class, args);
+    }
+
+}
---
+++ cicilismo/src/test/java/com/sofka/cicilismo/CicilismoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofka.cicilismo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicilismoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
