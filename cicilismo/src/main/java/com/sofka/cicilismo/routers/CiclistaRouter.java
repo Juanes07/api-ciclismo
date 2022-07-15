b0517c05a2abf09039392b97a2f3a4f9b811f12b
Juanes07 <juanesdm3@gmail.com>
2022-07-15 13:54:33 -0500

    UPDATE: Swagger
     se hace documentacion de los routes con swagger

--- cicilismo/src/main/java/com/sofka/cicilismo/routers/CiclistaRouter.java
+++ cicilismo/src/main/java/com/sofka/cicilismo/routers/CiclistaRouter.java
@@ -1,8 +1,11 @@
 package com.sofka.cicilismo.routers;
 
-import com.sofka.cicilismo.collection.Ciclista;
 import com.sofka.cicilismo.models.CiclistaDTO;
 import com.sofka.cicilismo.usecases.ciclistausecase.*;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -22,6 +25,8 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 public class CiclistaRouter {
 
     @Bean
+    @RouterOperation(beanClass = CrearCiclistaUseCase.class, beanMethod = "apply",
+    operation = @Operation(operationId = "CiclistaDto", summary = "crear ciclista"))
     public RouterFunction<ServerResponse> crearCiclista(CrearCiclistaUseCase useCase){
         Function<CiclistaDTO, Mono<ServerResponse>> trigger = ciclistaDTO -> useCase.apply(ciclistaDTO)
                 .flatMap(result -> ServerResponse.ok()
@@ -36,6 +41,15 @@ public class CiclistaRouter {
 
 
     @Bean
+    @RouterOperation(beanClass = CrearCiclistaUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "CiclistaDto", summary = "Actualizar Ciclista",
+                    parameters ={
+                            @Parameter(in = ParameterIn.PATH, name = "id", description = "Integer"),
+                            @Parameter(in = ParameterIn.PATH, name = "idEquipo", description = "Integer"),
+                            @Parameter(in = ParameterIn.PATH, name = "nombreCiclista",description = "String"),
+                            @Parameter(in= ParameterIn.PATH, name = "numeroCompetidor", description = "Integer"),
+                            @Parameter(in = ParameterIn.PATH, name = "nacionalidad",description = "String")
+                    }))
     public RouterFunction<ServerResponse> actualizarCiclista(ActualizarCiclistaPorIdUseCase useCase){
         Function<CiclistaDTO, Mono<ServerResponse>> trigger = ciclistaDTO -> useCase.apply(ciclistaDTO)
                 .flatMap(result -> ServerResponse.ok()
@@ -49,6 +63,9 @@ public class CiclistaRouter {
 
 
     @Bean
+    @RouterOperation(beanClass = CrearCiclistaUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "obtener ciclista por su ID", summary = "Obtener ciclista por su ID",
+            parameters = @Parameter(in = ParameterIn.PATH, name = "id",description = "Integer")))
     public RouterFunction<ServerResponse> obtenerCiclistaPorId(ObtenerCiclistaPorIdUseCase useCase){
         return  route(
                 GET("/obtenerciclista/{id}").and(accept(MediaType.APPLICATION_JSON)),
@@ -63,6 +80,8 @@ public class CiclistaRouter {
 
 
     @Bean
+    @RouterOperation(beanClass = CrearCiclistaUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "obtener ciclista por el id de su equipo", summary = "Obtener ciclista por el id de su equipo"))
     public RouterFunction<ServerResponse> obtenerCiclistaPorIdEquipo(ObtenerCiclistasPorIdEquipoUseCase useCase){
         return route(
                 GET("/obtenerciclistaporequipo/{id}").and(accept(MediaType.APPLICATION_JSON)),
@@ -77,6 +96,9 @@ public class CiclistaRouter {
 
 
     @Bean
+    @RouterOperation(beanClass = CrearCiclistaUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "Eliminar ciclista", summary = "Eliminar ciclista",
+                    parameters = {@Parameter(in = ParameterIn.PATH, name = "id", description = "Integer")}))
     public RouterFunction<ServerResponse> eliminarCiclistaPorId(EliminarCiclistaPorIdUseCase useCase){
         return route(
                 DELETE("/eliminarciclista/{id}").and(accept(MediaType.APPLICATION_JSON)),
@@ -89,6 +111,8 @@ public class CiclistaRouter {
 
 
     @Bean
+    @RouterOperation(beanClass = CrearCiclistaUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "obtener lista de ciclistas", summary = "Obtener lista de ciclistas"))
     public RouterFunction<ServerResponse> obtenerCiclistas(ObtenerCiclistasUseCase useCase){
         return route(
                 GET("/obtenerciclistas").and(accept(MediaType.APPLICATION_JSON)),
--- cicilismo/src/main/java/com/sofka/cicilismo/routers/EquipoRouter.java
+++ cicilismo/src/main/java/com/sofka/cicilismo/routers/EquipoRouter.java
@@ -2,8 +2,12 @@ package com.sofka.cicilismo.routers;
 
 
 import com.sofka.cicilismo.models.EquipoDTO;
+import com.sofka.cicilismo.usecases.ciclistausecase.CrearCiclistaUseCase;
 import com.sofka.cicilismo.usecases.equipousecase.*;
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import org.springdoc.core.annotations.RouterOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
@@ -15,7 +19,6 @@ import reactor.core.publisher.Mono;
 
 import java.util.function.Function;
 
-
 import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
@@ -24,7 +27,9 @@ import static org.springframework.web.reactive.function.server.RouterFunctions.r
 public class EquipoRouter {
 
     @Bean
-    public RouterFunction<ServerResponse> crearEquipo(CrearEquipoUseCase useCase){
+    @RouterOperation(beanClass = CrearEquipoUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "EquipoDTO", summary = "Crear Equipo", tags = {"Equipo"}))
+    public RouterFunction<ServerResponse> crearEquipo(CrearEquipoUseCase useCase) {
         Function<EquipoDTO, Mono<ServerResponse>> trigger = equipoDTO -> useCase.apply(equipoDTO)
                 .flatMap(result -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
@@ -38,7 +43,14 @@ public class EquipoRouter {
 
 
     @Bean
-    public RouterFunction<ServerResponse> actualizarEquipo(ActualizarEquipoPorIdUseCase useCase){
+    @RouterOperation(beanClass = CrearEquipoUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "EquipoDTO", summary = "Actualizar Equipo", tags = {"Equipo"},
+                    parameters = {@Parameter(in = ParameterIn.PATH, name = "id", description = "Integer"),
+                            @Parameter(in = ParameterIn.PATH, name = "nombreEquipo", description = "String"),
+                            @Parameter(in = ParameterIn.PATH, name = "codigoEquipo", description = "String"),
+                            @Parameter(in = ParameterIn.PATH, name = "pais", description = "String")})
+    )
+    public RouterFunction<ServerResponse> actualizarEquipo(ActualizarEquipoPorIdUseCase useCase) {
         Function<EquipoDTO, Mono<ServerResponse>> trigger = equipoDTO -> useCase.apply(equipoDTO)
                 .flatMap(result -> ServerResponse.ok()
                         .contentType(MediaType.APPLICATION_JSON)
@@ -50,18 +62,23 @@ public class EquipoRouter {
     }
 
     @Bean
-    public RouterFunction<ServerResponse> eliminarEquipo(EliminarEquipoUseCase useCase){
+    @RouterOperation(beanClass = CrearEquipoUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "Eliminar Equipo", summary = "Eliminar equipo", tags = {"Equipo"},
+            parameters = @Parameter(in = ParameterIn.PATH, name = "id",description = "Integer")))
+    public RouterFunction<ServerResponse> eliminarEquipo(EliminarEquipoUseCase useCase) {
         return route(
                 DELETE("/eliminarequipo/{id}").and(accept(MediaType.APPLICATION_JSON)),
                 request -> ServerResponse.accepted()
                         .contentType(MediaType.APPLICATION_JSON)
-                        .body(BodyInserters.fromPublisher(useCase.apply(request.pathVariable("id")),void.class))
+                        .body(BodyInserters.fromPublisher(useCase.apply(request.pathVariable("id")), void.class))
 
         );
     }
 
     @Bean
-    public RouterFunction<ServerResponse> obtenerEquipos(ObtenerEquiposUseCase useCase){
+    @RouterOperation(beanClass = CrearEquipoUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "Obtener todas los Equipo", summary = "Obtener todas los Equipos", tags = {"Equipo"}))
+    public RouterFunction<ServerResponse> obtenerEquipos(ObtenerEquiposUseCase useCase) {
         return route(
                 GET("/obtenerEquipos").and(accept(MediaType.APPLICATION_JSON)),
                 request -> ServerResponse.ok()
@@ -71,7 +88,10 @@ public class EquipoRouter {
     }
 
     @Bean
-    public RouterFunction<ServerResponse> obtenerEquipoPorId(ObtenerEquipoPorIdUseCase useCase){
+    @RouterOperation(beanClass = CrearEquipoUseCase.class, beanMethod = "apply",
+            operation = @Operation(operationId = "Obtener Equipo por id", summary = "Obtener Equipo por id", tags = {"Equipo"},
+            parameters = {@Parameter(in = ParameterIn.PATH, name = "id", description = "Integer")}))
+    public RouterFunction<ServerResponse> obtenerEquipoPorId(ObtenerEquipoPorIdUseCase useCase) {
         return route(
                 GET("/obtenerequipo/{id}").and(accept(MediaType.APPLICATION_JSON)),
                 request -> ServerResponse.ok()
@@ -80,7 +100,6 @@ public class EquipoRouter {
                                 EquipoDTO.class))
         );
     }
-
 
 
 }
